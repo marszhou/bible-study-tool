a4f57d5fa5e04bf82ea8102646d9954f6bf5339c
mars <mars@huoban.com>
2017-10-23 11:50:10 +0800

    test antd

--- app/components/Home.js
+++ app/components/Home.js
@@ -3,15 +3,15 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import styles from './Home.css';
 import NavPane from './NavPane';
+import { DatePicker } from 'antd';
 
 export default class Home extends Component {
   render() {
     return (
       <div>
         <div className={styles.container} data-tid="container">
-          <h2>Home</h2>
-          <Link to="/counter">to Counter</Link>
           <NavPane />
+          <DatePicker />
         </div>
       </div>
     );
