fb5d2d03158e5ed2b340b387973170163571cfa3
mars <mars@huoban.com>
2017-10-23 23:16:49 +0800

    sidebar

--- app/components/SideBar.jsx
+++ app/components/SideBar.jsx
@@ -1,5 +1,34 @@
-import React from 'react'
+import {React, PropTypes} from 'app/bootstrap'; // eslint-disable-line
+import styles from "./SideBar.css";
+import SideBarItem, { ItemPropType } from "./SiderBarItem";
 
-export default () => {
-  return (<div>1</div>)
+export default class extends React.Component {
+  static propTypes = {
+    primaryItems: PropTypes.arrayOf(ItemPropType),
+    toolItems: PropTypes.arrayOf(ItemPropType),
+    style: PropTypes.object,
+  };
+
+  static defaultProps = {
+    style: {},
+    primaryItems: [],
+    toolItems: [],
+  };
+
+  render() {
+    return (
+      <div className={styles.sidebar} style={this.props.style}>
+        <div className={styles.sidebarPrimaryItems}>
+          {this.props.primaryItems.map(item => (
+            <SideBarItem item={item} type="primary" highlighted={false} />
+          ))}
+        </div>
+        <div className={styles.sidebarToolItems}>
+          {this.props.toolItems.map(item => (
+            <SideBarItem item={item} type="tool" />
+          ))}
+        </div>
+      </div>
+    );
+  }
 }
---
+++ app/components/SiderBarItem.jsx
@@ -0,0 +1,24 @@
+import {React, PropTypes} from 'app/bootstrap'; // eslint-disable-line
+
+export const ItemPropType = PropTypes.shape({
+  text: PropTypes.string,
+  icon: PropTypes.string,
+});
+
+const SideBarItem = () => {
+  return <div>item</div>;
+};
+
+export const ItemTypes = ['primary', 'tool'];
+
+SideBarItem.propTypes = {
+  item: ItemPropType.isRequired,
+  highlighted: PropTypes.bool,
+  type: PropTypes.oneOf(ItemTypes)
+}
+
+SideBarItem.defaultProps = {
+  highlighted: false,
+  type: ItemTypes[0]
+}
+export default SideBarItem;
