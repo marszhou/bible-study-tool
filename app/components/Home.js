dad54b7c738acc77df64c570f60ab0a793610bfd
mars <mars@huoban.com>
2017-10-23 13:57:33 +0800

    test antd

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,18 +1,52 @@
 // @flow
-import React, { Component } from 'react';
+import React, { Component } from "react";
 // import { Link } from 'react-router-dom';
-import { DatePicker,  Modal, Button } from 'antd';
-import styles from './Home.css';
-import NavPane from './NavPane';
+import { DatePicker, Button, Modal } from "antd";
+import styles from "./Home.css";
+import NavPane from "./NavPane";
 
 export default class Home extends Component {
+  props: {};
+  state: {visible: boolean};
+  constructor(props: {}) {
+    super(props);
+    this.state = {
+      visible: false
+    }
+  }
+
+  showModal = () => {
+    this.setState({visible: true})
+    // console.log(1)
+  };
+
+  handleOk = () => {
+    this.setState({visible: false})
+  };
+
+  handleCancel = () => {
+    this.setState({visible: false})
+  };
+
   render() {
     return (
       <div>
         <div className={styles.container} data-tid="container">
           <NavPane />
           <DatePicker />
-
+          <Button type="primary" onClick={this.showModal}>
+            Open
+          </Button>
+          <Modal
+            title="Basic Modal"
+            visible={this.state.visible}
+            onOk={this.handleOk}
+            onCancel={this.handleCancel}
+          >
+            <p>Some contents...</p>
+            <p>Some contents...</p>
+            <p>Some contents...</p>
+          </Modal>
         </div>
       </div>
     );
