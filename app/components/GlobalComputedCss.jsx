a76c2ec9cbbc2347025f33c756a236b34611f319
mars <mars@huoban.com>
2017-10-24 00:19:51 +0800

    handle resize

--- app/components/GlobalComputedCss.jsx
+++ app/components/GlobalComputedCss.jsx
@@ -1,13 +1,29 @@
 import React from 'react';
 
 export default class extends React.Component {
+  componentWillMount() {
+    console.log(window);
+    window.addEventListener('resize', this.handleSizeChange)
+  }
+
+  componentWillUnmount() {
+
+  }
+
+  handleSizeChange = () => {
+    this.forceUpdate()
+  }
+
   render() {
     return (
       <style>
         {
           `
-          .my-class {
-            font-size: 20px;
+          .client-height {
+            height: ${window.document.documentElement.clientHeight}px;
+          }
+          .client-width {
+            width: ${window.document.documentElement.clientWidth}px;
           }
           `
         }
