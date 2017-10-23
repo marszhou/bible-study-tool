cd0a46af5243e1e26073a8ae4e67818a3ac7d36e
mars <mars@huoban.com>
2017-10-23 23:17:24 +0800

    for test

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,56 +1,27 @@
 // @flow
 import React, { Component } from "react";
 // import { Link } from 'react-router-dom';
-import { DatePicker, Button, Modal } from "antd";
+// import { DatePicker, Button, Modal } from "antd";
 import styles from "./Home.css";
-import NavPane from "./NavPane";
-import GlobalComputedCss from './GlobalComputedCss';
+// import NavPane from "./NavPane";
+// import GlobalComputedCss from './GlobalComputedCss';
+import SideBar from "./SideBar";
 
 export default class Home extends Component {
   props: {};
-  state: {visible: boolean};
+  state: { visible: boolean };
   constructor(props: {}) {
     super(props);
     this.state = {
-      visible: false
-    }
+      visible: false,
+    };
   }
-
-  showModal = () => {
-    this.setState({visible: true})
-    // console.log(1)
-  };
-
-  handleOk = () => {
-    this.setState({visible: false})
-  };
-
-  handleCancel = () => {
-    this.setState({visible: false})
-  };
 
   render() {
     return (
       <div>
         <div className={styles.container} data-tid="container">
-          <GlobalComputedCss />
-          <div className="my-class">mmm</div>
-          <NavPane />
-          <DatePicker />
-          <Button type="primary" onClick={this.showModal}>
-            Open
-          </Button>
-          <Modal
-            title="Basic Modal"
-            visible={this.state.visible}
-            onOk={this.handleOk}
-            onCancel={this.handleCancel}
-          >
-            <p>Some contents...</p>
-            <p>Some contents...</p>
-            <p>Some contents...</p>
-          </Modal>
-          <i className="fa fa-address-book" aria-hidden="true" />
+          <SideBar />
         </div>
       </div>
     );
