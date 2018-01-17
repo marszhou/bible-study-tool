4413852ac1eece351b1a0dec623731dd7c85b1f5
mars <mars@huoban.com>
2018-01-18 06:50:47 +0800

    samples

--- app/components/Home.jsx
+++ app/components/Home.jsx
@@ -2,71 +2,9 @@ import { React, PropTypes, cx, Component, Route, Link } from 'app/bootstrap' // 
 import styles from './Home.css'
 import GlobalComputedCss from './GlobalComputedCss'
 import SideBar from './side-bar/SideBar'
-
-const sampleRoutes = [
-  {
-    path: '/bible-selector-demo',
-    exact: true,
-    name: 'bible-selector-demo',
-    component: require('../pages/BibleSelectorPage')
-  },
-  {
-    path: '/read-demo',
-    exact: true,
-    name: 'read-demo',
-    component: require('../pages/ReadPage')
-  },
-  {
-    path: '/icon-page',
-    exact: true,
-    name: 'icon-page',
-    component: require('../pages/IconPage')
-  },
-  {
-    path: '/split-pane-page',
-    exact: true,
-    name: 'split-pane-page',
-    component: require('../pages/SplitPanePage')
-  },
-  {
-    path: '/sqlite-page',
-    exact: true,
-    name: 'sqlite-page',
-    component: require('../pages/SqliteDemoPage')
-  },
-  {
-    path: '/samples/common-components-page',
-    exact: true,
-    name: 'common-components',
-    component: require('../pages/samples/CommomComponentPage')
-  }
-]
+import Samples from './Samples'
 
 export default class Home extends Component {
-  renderSampleRoutes() {
-    return [
-      <div
-        key="menu"
-        style={{ position: 'fixed', top: 0, right: 0, zIndex: 1 }}
-      >
-        <ul>
-          <li>
-            <Link to="/">home</Link>
-          </li>
-          {sampleRoutes.map(route => (
-            <li key={route.name}>
-              <Link to={route.path}>{route.name}</Link>
-            </li>
-          ))}
-        </ul>
-      </div>,
-      ...sampleRoutes.map(route => {
-        const params = { ...route, key: route.name }
-        return <Route {...params} />
-      })
-    ]
-  }
-
   renderSideBar() {
     return (
       <SideBar
@@ -115,7 +53,9 @@ export default class Home extends Component {
       <div>
         <div className={styles.container} data-tid="container">
           {this.renderSideBar()}
-          <div className="content">{this.renderSampleRoutes()}</div>
+          <div className="content">
+            <Samples />
+          </div>
           <GlobalComputedCss />
         </div>
       </div>
