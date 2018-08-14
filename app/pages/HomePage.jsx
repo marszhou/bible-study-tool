16691d5251c601646eacb4112ed9cea6d5906734
matt zhou <matt@cruelyouth.com>
2018-08-14 11:12:56 +0800

    refactor main

--- app/pages/HomePage.jsx
+++ app/pages/HomePage.jsx
@@ -1,10 +1,36 @@
-import React, { Component } from 'react';
-import Main from '../components/Main';
+import React, { Component } from 'react'
+import Main from '../components/Main'
+import GlobalComputedCss from '../components/GlobalComputedCss'
 
 export default class HomePage extends Component {
+  state = {}
+
+  componentDidMount() {
+    const cssLoaded = e => this.setState({ cssLoaded: true })
+
+    Promise.all(
+      Array.prototype.slice
+        .call(document.querySelectorAll('link[rel="stylesheet"]'))
+        .map(
+          el =>
+            new Promise((resolve, reject) => {
+              el.addEventListener('load', () => {
+                resolve()
+              })
+            })
+        )
+    )
+      .then(cssLoaded)
+      .catch(cssLoaded)
+  }
+
   render() {
-    return (
-      <Main />
-    );
+    const { cssLoaded } = this.state
+    return cssLoaded ? (
+      <div>
+        <GlobalComputedCss />
+        <Main />
+      </div>
+    ) : null
   }
 }
