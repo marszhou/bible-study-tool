5073d54de2f0fa43c5beb39a47ae8864e2cf4610
matt zhou <matt@cruelyouth.com>
2018-08-14 10:33:50 +0800

    remove

--- app/pages/BiblePage.jsx
+++ app/pages/BiblePage.jsx
@@ -4,7 +4,6 @@ import { connect } from 'react-redux'
 import * as layoutActions from '../actions/layout'
 import { layoutSelectors } from '../reducers'
 import { TabPane } from '../components/tabs'
-import getConnectedBibleView from '../components/bible-view/BibleView'
 
 class BiblePage extends Component {
   componentWillMount() {
