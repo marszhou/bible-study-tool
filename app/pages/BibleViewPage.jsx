df4cd88e3a7cba8bacec33fa36603e3b82555f7f
matt zhou <matt@cruelyouth.com>
2018-08-14 16:44:36 +0800

    upgrade semantic

--- app/pages/BibleViewPage.jsx
+++ app/pages/BibleViewPage.jsx
@@ -1,18 +1,40 @@
 import React, { Component } from 'react'
+import { Breadcrumb, Button, Grid, Popup, Label, Icon } from 'semantic-ui-react'
+import styles from './BibleViewPage.css'
 
 class BibleViewPage extends Component {
   render() {
     const { match } = this.props
     return (
-      <div>
-        match.params.tabId:
-        {match.params.tabId}
-        <br />
-        match.params.bookId
-        {match.params.bookId}
-        <br />
-        match.params.chapterIndex
-        {match.params.chapterIndex}
+      <div className={styles.bibleViewWrapper}>
+        <Breadcrumb>
+          <Breadcrumb.Section link>
+            <Popup
+              trigger={<Label content="111" />}
+              className={styles.bibleSelectorPopup}
+              content={
+                <div style={{ background: 'red', width: 500, height: 500 }} />
+              }
+              on="click"
+              verticalOffset={-10}
+            />
+          </Breadcrumb.Section>
+          <Breadcrumb.Divider />
+          <Breadcrumb.Section link>Store</Breadcrumb.Section>
+          <Breadcrumb.Divider />
+          <Breadcrumb.Section active>T-Shirt</Breadcrumb.Section>
+        </Breadcrumb>
+
+        <div>
+          match.params.tabId:
+          {match.params.tabId}
+          <br />
+          match.params.bookId:
+          {match.params.bookId}
+          <br />
+          match.params.chapterIndex:
+          {match.params.chapterIndex}
+        </div>
       </div>
     )
   }
