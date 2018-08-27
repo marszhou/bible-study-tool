67dfd2d6d4257ce0a0b78232b97142eeec97fd6c
mars@windows <marszhou@github.com>
2018-08-27 16:37:52 +0800

    for windows build

--- app/main.dev.js
+++ app/main.dev.js
@@ -27,16 +27,17 @@ if (process.env.NODE_ENV === 'development' || process.env.DEBUG_PROD === 'true')
 }
 
 const installExtensions = async () => {
-  const installer = require('electron-devtools-installer');
-  const forceDownload = !!process.env.UPGRADE_EXTENSIONS;
-  const extensions = [
-    'REACT_DEVELOPER_TOOLS',
-    'REDUX_DEVTOOLS'
-  ];
+  return Promise.resolve()
+  // const installer = require('electron-devtools-installer');
+  // const forceDownload = !!process.env.UPGRADE_EXTENSIONS;
+  // const extensions = [
+  //   'REACT_DEVELOPER_TOOLS',
+  //   'REDUX_DEVTOOLS'
+  // ];
 
-  return Promise
-    .all(extensions.map(name => installer.default(installer[name], forceDownload)))
-    .catch(console.log);
+  // return Promise
+  //   .all(extensions.map(name => installer.default(installer[name], forceDownload)))
+  //   .catch(console.log);
 };
 
 
