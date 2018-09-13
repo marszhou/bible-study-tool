e4a65c6f1fcde8c9c45a82035712525f4b106f98
matt zhou <matt@cruelyouth.com>
2018-09-13 13:22:20 +0800

    toolbar ui

--- app/components/bible-view/Toolbar.jsx
+++ app/components/bible-view/Toolbar.jsx
@@ -1,14 +1,25 @@
-import React from 'react';
+import React from 'react'
 import styles from './Toolbar.css'
+import { Button, Icon } from 'semantic-ui-react'
 
 const Toolbar = () => {
   return (
     <div className={styles.toolbarFrame}>
       <div className={styles.toolbar}>
-22222
+        <Button.Group>
+          <Button icon>
+            <Icon name="copy" />
+          </Button>
+          <Button icon>
+            <Icon name="thumbtack" />
+          </Button>
+          <Button icon>
+            <Icon name="edit outline" />
+          </Button>
+        </Button.Group>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Toolbar;
+export default Toolbar
