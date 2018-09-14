96e113b5f48506eccfac371b83bed78775197f89
matt zhou <matt@cruelyouth.com>
2018-09-14 16:23:31 +0800

    clip board

--- app/components/bible-view/Toolbar.jsx
+++ app/components/bible-view/Toolbar.jsx
@@ -2,33 +2,49 @@ import React from 'react'
 import styles from './Toolbar.css'
 import { Button, Icon } from 'semantic-ui-react'
 import { connect } from 'react-redux'
-import {toolbarSelectors} from 'app/reducers'
+import { toolbarSelectors } from 'app/reducers'
 import cx from 'classnames'
 
-const Toolbar = ({dontDisturb}) => {
-  const classNames = {
-    [styles.toolbarFrame]: true,
-    [styles.dontDisturb]: dontDisturb
+class Toolbar extends React.Component {
+  constructor(props) {
+    super(props)
   }
-  return (
-    <div className={cx(classNames)}>
-      <div className={styles.toolbar}>
-        <Button.Group>
-          <Button icon>
-            <Icon name="copy" />
-          </Button>
-          <Button icon>
-            <Icon name="thumbtack" />
-          </Button>
-          <Button icon>
-            <Icon name="edit outline" />
-          </Button>
-        </Button.Group>
+
+  handleCopy = () => {
+    const { clipboard } = require('electron')
+
+    clipboard.writeText('Electron 示例!')
+  }
+
+  render() {
+    const { dontDisturb } = this.props
+    const classNames = {
+      [styles.toolbarFrame]: true,
+      [styles.dontDisturb]: dontDisturb
+    }
+    return (
+      <div className={cx(classNames)}>
+        <div className={styles.toolbar}>
+          <Button.Group>
+            <Button icon>
+              <Icon name="copy" onClick={this.handleCopy} />
+            </Button>
+            <Button icon>
+              <Icon name="thumbtack" />
+            </Button>
+            <Button icon>
+              <Icon name="edit outline" />
+            </Button>
+          </Button.Group>
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
-export default connect(state => ({
-  dontDisturb: toolbarSelectors.getDontDisturb(state)
-}), {})(Toolbar)
+export default connect(
+  state => ({
+    dontDisturb: toolbarSelectors.getDontDisturb(state)
+  }),
+  {}
+)(Toolbar)
